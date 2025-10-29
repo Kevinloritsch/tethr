1a3973bea58a14b03e642dbbe0c60f95c129bf84
Charleen Chen <charleenc485@gmail.com>
2025-10-28 23:38:41 -0700

    uh idk if it works yet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,61 @@
-import { View } from 'react-native';
+'use client'
+
+import { View, Button, TextInput } from 'react-native';
 import { AppText } from '@/components/apptext';
+import { useState } from 'react';
+import { supabase } from '@/lib/supabase';
 
 export default function IndexScreen() {
+  const [email, setEmail] = useState('')
+  const [otp, setOtp] = useState('')
+  const [otpSent, setOtpSent] = useState(false)
+
+  const testOTP = async () => {
+    console.log('Sending OTP...')
+    
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email,
+    })
+
+    if (error) {
+      console.error('Error:', error.message);
+    } else {
+      console.log('Success! Check your email');
+      setOtpSent(true);
+    }
+  }
+
+  const verifyOTP = async () => {
+    const { data, error } = await supabase.auth.verifyOtp({
+      email: email,
+      token: otp,
+      type: 'email',
+    })
+
+    if (error) {
+      console.error('Error verifying OTP:', error);
+    } else{
+      console.log('User authenticated:', data);
+    }
+  }
+
   return (
     <View className="flex-1 justify-center p-4">
+      <TextInput
+        style={{ borderWidth: 1, padding: 10, marginBottom: 20 }}
+        value={email}
+        onChangeText={setEmail}
+        placeholder="tethr@gmail.com"
+      />
+       <Button title="Send Test OTP" onPress={testOTP} />
+      <TextInput
+            style={{ borderWidth: 1, padding: 10, marginBottom: 20 }}
+            value={otp}
+            onChangeText={setOtp}
+            placeholder="123456"
+            keyboardType="number-pad"
+          />
+      <Button title="Verify OTP" onPress={verifyOTP} />
       <AppText center>
         Open up <AppText bold>app/index.tsx</AppText> to start working on your app!
       </AppText>
