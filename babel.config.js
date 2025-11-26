2d5371c05d516db32f663be4bc03edd2272c2f40
qhgill <qhgill2@gmail.com>
2025-11-26 14:34:04 -0800

    fix npm start not working

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,10 @@
 module.exports = function (api) {
   const isTest = api.env('test');
-  api.cache(true);
 
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: 'nativewind',
-        },
-      ],
-      'nativewind/babel',
-    ],
-    plugins: [
-      'module:react-native-dotenv',
-      ...(isTest ? [] : ['react-native-reanimated/plugin', 'react-native-worklets/plugin']),
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins: [!isTest && 'react-native-reanimated/plugin', 'module:react-native-dotenv'].filter(
+      Boolean
+    ),
   };
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-native/extend-expect';
 import { jest } from '@jest/globals';
 
-// prevent Reanimated warnings
 jest.mock('react-native-reanimated', () => require('__mocks__/react-native-reanimated'));
 jest.mock('react-native-worklets', () => require('__mocks__/react-native-worklets'));
