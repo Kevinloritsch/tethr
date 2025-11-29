d56d4e7f0994fd4a64fe60dfbeaac141237eb9e5
Kevin Loritsch <kloritsch58@gmail.com>
2025-11-29 12:06:33 -0800

    fixed spacing

--- src/app/main/groups/[groupId]/index.tsx
+++ src/app/main/groups/[groupId]/index.tsx
@@ -163,7 +163,7 @@ const GroupPage = () => {
             />
           </TouchableOpacity>
         </View>
-        <View className="flex-row items-center justify-between px-[5vw] pt-4">
+        <View className="flex-row items-center justify-between px-[5vw] pb-4 pt-2">
           <Text className="text-3xl font-bold leading-none text-white">{groupNameState}</Text>
           <Pressable className="rounded-3xl bg-tethr-purple/40 px-4 py-2" onPress={leaveGroup}>
             <Text className="text-white">Leave Group</Text>
